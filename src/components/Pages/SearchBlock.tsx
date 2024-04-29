76892f99f7317f7a6ebe1af28bcfc66dda684f41
xiracuko <matvey.a.a.7@gmail.com>
2024-04-29 21:15:17 +0300

    build: adding the SearchBlock.tsx component and completely styling it

---
+++ src/components/Pages/SearchBlock.tsx
@@ -0,0 +1,9 @@
+function SearchBlock() {
+  return (
+    <div className="searchBlock">
+      <p className="searchBlock--text">Find out the weather in your city!</p>
+    </div>
+  )
+}
+
+export default SearchBlock
