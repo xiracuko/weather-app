e4a76677ea1d578746d2bc9f1e9efb16e71cd8ac
xiracuko <matvey.a.a.7@gmail.com>
2024-04-30 23:12:59 +0300

    feature: adding react-router-dom v6 library and initializing it in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,27 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import WeatherBlock from './components/Pages/WeatherBlock';
 
 const container = (document.getElementById("root")!);
 const root = ReactDOM.createRoot(container);
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/weather",
+        element: <WeatherBlock time={0} icon={''} temp_c={0} text={''} city={''} country={''} humidity={''} windSpeed={''} />
+      }
+    ]
+  }
+])
+
 root.render(
   <Provider store={store}>
-    <App />
+    <RouterProvider router={router} />
   </Provider>
 );
